4e1bc8625f3c675d039f3f6539c624b076786867
behzadsoleimani <behzadsoleimani97@gmail.com>
2022-08-16 14:11:12 +0430

    create Header and wrappers

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from "./pages/home";
+import Explore from "./pages/explore";
+import Saved from "./pages/saved";
+import Profile from "./pages/profile";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/"  element={<Home />} />
+        <Route path="Explore" element={<Explore />} />
+        <Route path="Saved" element={<Saved />} />
+        <Route path="Profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
